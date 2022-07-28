f3cf31a113f476bc0be84e2fba87baa7feab39e1
DudeX Dude <dudex992@gmail.com>
2022-07-28 17:47:36 +0300

    Remove exists rule for login request

--- app/Http/Requests/Brans/Auth/LoginRequest.php
+++ app/Http/Requests/Brans/Auth/LoginRequest.php
@@ -14,7 +14,7 @@ class LoginRequest extends FormRequest
     public function rules()
     {
         return [
-            'email' => 'required|exists:users,email',
+            'email' => 'required',
             'password' => 'required',
         ];
     }
