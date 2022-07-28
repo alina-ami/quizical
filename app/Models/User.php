a6d2097840ab7ffe4f4491f63616f7d3a1c2a7a9
Alina Gorgovan <alina@adoreme.com>
2022-07-28 15:14:40 +0300

    remove fillable

--- app/Models/User.php
+++ app/Models/User.php
@@ -16,22 +16,12 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-        'google_id',
-    ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    // protected $fillable = [
+    //     'name',
+    //     'email',
+    //     'password',
+    //     'google_id',
+    // ];
 
     /**
      * The attributes that should be cast.
