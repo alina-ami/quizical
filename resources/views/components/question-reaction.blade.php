b49107bc0d1207f35aeb093effc0c2a32e54bdb6
Alina Gorgovan <alina@adoreme.com>
2022-07-29 12:27:56 +0300

    q sentiment

--- resources/views/components/question-reaction.blade.php
+++ resources/views/components/question-reaction.blade.php
@@ -1,21 +1,27 @@
-@switch(strlen($value) % 3)
-    @case(0)
-        <a class="btn btn-rounded btn-outline-success mb-0 me-2 text-xs btn-sm align-items-center justify-content-center">
-            Positive
-        </a>
-    @break
+ @switch($value)
+ @case('positive')
+ <a class="btn btn-rounded btn-outline-success mb-0 me-2 text-xs btn-sm align-items-center justify-content-center">
+   Positive
+ </a>
+ @break
 
-    @case(1)
-        <a class="btn btn-rounded btn-outline-default mb-0 me-2 text-xs btn-sm align-items-center justify-content-center">
-            Neutral
-        </a>
-    @break
+ @case('neutral')
+ <a class="btn btn-rounded btn-outline-info mb-0 me-2 text-xs btn-sm align-items-center justify-content-center">
+   Neutral
+ </a>
+ @break
 
-    @case(2)
-        <a class="btn btn-rounded btn-outline-danger mb-0 me-2 text-xs btn-sm align-items-center justify-content-center">
-            Negative
-        </a>
-    @break
+ @case('negative')
+ <a class="btn btn-rounded btn-outline-danger mb-0 me-2 text-xs btn-sm align-items-center justify-content-center">
+   Negative
+ </a>
+ @break
 
-    @default
-@endswitch
+ @case('not_applicable')
+ <a class="btn btn-rounded btn-outline-default mb-0 me-2 text-xs btn-sm align-items-center justify-content-center">
+   No answers yet
+ </a>
+ @break
+
+ @default
+ @endswitch
