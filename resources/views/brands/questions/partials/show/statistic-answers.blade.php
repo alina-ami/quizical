215dc5437caf8b24ffd5dc01f92f65dbee2fcad5
Alina Gorgovan <alina@adoreme.com>
2022-07-29 15:03:25 +0300

    add cache for word cloud

--- resources/views/brands/questions/partials/show/statistic-answers.blade.php
+++ resources/views/brands/questions/partials/show/statistic-answers.blade.php
@@ -3,7 +3,7 @@
         <h6 class="mb-0">Keywords Cloud</h6>
     </div>
     <div class="card-body px-0">
-        <img class="img-fluid"  src="data:image/png;base64,{{ base64_encode($topKeywordsImage) }}" />
+        <img class="img-fluid"  src="data:image/png;base64,{{ $topKeywordsImage }}" />
     </div>
 </div>
 
